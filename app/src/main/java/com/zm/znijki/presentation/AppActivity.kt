01a2b6443a6f746e0cf9270e19ac3a769ae80b66
Yevhen <moskalenko.16.11.1996@gmail.com>
2022-02-19 15:52:14 +0300

    added base configurations

---
+++ app/src/main/java/com/zm/znijki/presentation/AppActivity.kt
@@ -0,0 +1,13 @@
+package com.zm.znijki.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zm.znijki.R
+
+class AppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+    }
+}
